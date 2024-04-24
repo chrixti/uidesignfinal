92531cd8cc5e3ea4db9a535a183a69b60bc0a7e7
christine <107821719+chrixti@users.noreply.github.com>
2024-04-24 04:56:40 -0400

    updated layout

--- compostedu/server.py
+++ compostedu/server.py
@@ -4,23 +4,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Changed from 'template/index.html'
+    return render_template('welcome.html')  
+
+@app.route('/welcome')
+def welcome():
+   return render_template('welcome.html')  
 
 @app.route('/basics')
 def basics():
-    return render_template('basics.html')  # Changed from 'template/basics.html'
+    return render_template('basics.html')  
 
 @app.route('/home')
 def home():
-    return render_template('home.html')  # Changed from 'template/home.html'
+    return render_template('home.html')  
 
 @app.route('/backyard')
 def backyard():
-    return render_template('backyard.html')  # Changed from 'template/backyard.html'
+    return render_template('backyard.html')  
 
 @app.route('/quiz')
 def quiz():
-    return render_template('quiz.html')  # Changed from 'template/quiz.html'
+    return render_template('quiz.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
